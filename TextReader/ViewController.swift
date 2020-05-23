e14f363237af544fa4bdfe8f751dd041bae288d2
Eric Internicola <intere@gmail.com>
2020-05-23 10:58:41 -0600

    Initial Commit

---
+++ TextReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextReader
+//
+//  Created by Eric Internicola on 5/23/20.
+//  Copyright © 2020 Eric Internicola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
