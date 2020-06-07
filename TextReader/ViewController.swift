e5528bbe6163d3183cda43fb38b292984c3464e3
Eric Internicola <intere@gmail.com>
2020-06-06 22:01:57 -0600

    Improving the UX a bit.

--- TextReader/ViewController.swift
+++ TextReader/ViewController.swift
@@ -16,9 +16,9 @@ import VisionKit
 class ViewController: UIViewController {
     
     let textView = UITextView(withText: "Click the scan button to take a photo and read the text from that photo.  The text will appear here.")
-    let scanButton = UIButton(withText: "Scan")
-    let addPhotoButton = UIButton(withText: "Photo")
-    let copyButton = UIButton(withText: "Copy")
+    let scanButton = UIButton(withText: "Scan Text")
+    let addPhotoButton = UIButton(withText: "Text from Photo")
+    let copyButton = UIButton(withText: "Copy Text")
     let activityIndicator = UIActivityIndicatorView(style: .large)
     
     let imagePicker = UIImagePickerController()
@@ -98,9 +98,9 @@ extension ViewController: UIImagePickerControllerDelegate {
         readText(from: [pickedImage])
     }
 
-
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
         print("cancel is clicked")
+        picker.dismiss(animated: true, completion: nil)
     }
 }
 
@@ -209,14 +209,13 @@ private extension ViewController {
     
     func buildView() {
         [textView, scanButton, addPhotoButton, copyButton, activityIndicator].forEach { view.addSubview($0) }
-        [scanButton, addPhotoButton, copyButton].forEach { $0.setTitleColor(.blue, for: .normal) }
         
         scanButton.addTarget(self, action: #selector(tappedScan(_:)), for: .touchUpInside)
         addPhotoButton.addTarget(self, action: #selector(tappedPhoto(_:)), for: .touchUpInside)
         copyButton.addTarget(self, action: #selector(tappedCopy(_:)), for: .touchUpInside)
         
         constrain(view, textView, scanButton, addPhotoButton, copyButton, activityIndicator) { view, textView, scanButton, addPhotoButton, copyButton,  activityIndicator in
-            textView.top == view.safeAreaLayoutGuide.top
+            textView.top == view.safeAreaLayoutGuide.top + 16
             textView.left == view.left + 16
             textView.right == view.right - 16
             textView.bottom == copyButton.top - 16
@@ -224,13 +223,16 @@ private extension ViewController {
             activityIndicator.centerX == view.centerX
             activityIndicator.centerY == view.centerY
             
-            copyButton.centerX == view.centerX
+            copyButton.left == view.left + 16
+            copyButton.right == view.right - 16
             copyButton.bottom == addPhotoButton.top - 16
             
-            addPhotoButton.centerX == view.centerX
+            addPhotoButton.left == view.left + 16
+            addPhotoButton.right == view.right - 16
             addPhotoButton.bottom == scanButton.top - 16
             
-            scanButton.centerX == view.centerX
+            scanButton.left == view.left + 16
+            scanButton.right == view.right - 16
             scanButton.bottom == view.safeAreaLayoutGuide.bottom - 16
         }
         
@@ -238,21 +240,35 @@ private extension ViewController {
     }
 }
 
+// MARK: - UITextView(withText:)
 
 extension UITextView {
     
-    convenience init(withText text: String) {
+    convenience init(withText text: String, fontSize: CGFloat = 24) {
         self.init(frame: .zero)
         self.text = text
+        font = .systemFont(ofSize: fontSize)
+        
+        layer.borderColor = UIColor.gray.cgColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 4
     }
     
 }
 
+// MARK: - UIButton(withText:)
+
 extension UIButton {
     
-    convenience init(withText text: String) {
+    convenience init(withText text: String, titleColor: UIColor = .blue) {
         self.init(type: .custom)
         setTitle(text, for: .normal)
+        setTitleColor(titleColor, for: .normal)
+        
+        contentEdgeInsets = UIEdgeInsets(top: 12, left: 24, bottom: 12, right: 24)
+        layer.borderColor = titleColor.cgColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 8
     }
     
 }
